a548bfec5b630a937028aec6b25e033d63359079
muniguedio <moniquedoidamira1@gmail.com>
2025-08-31 22:24:38 -0300

    Add files via upload

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = ' https://mairahandrade.github.io/portfolio/';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
